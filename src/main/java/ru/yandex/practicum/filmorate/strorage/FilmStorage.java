0e8dda3f745291dfd4ecf5028d7c8a55e0d64f92
Temlesk <Teml1010>
2026-03-24 20:32:42 +0300

    commit №4 add-friends-likes

--- src/main/java/ru/yandex/practicum/filmorate/strorage/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/strorage/FilmStorage.java
@@ -1,12 +1,15 @@
 package ru.yandex.practicum.filmorate.strorage;
 
 import ru.yandex.practicum.filmorate.model.Film;
-
 import java.util.List;
 
 public interface FilmStorage {
+
     Film create(Film film);
+
     Film update(Film film);
+
     List<Film> getAll();
+
     Film get(long filmId);
 }
--- src/main/java/ru/yandex/practicum/filmorate/strorage/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/strorage/UserStorage.java
@@ -6,9 +6,14 @@ import ru.yandex.practicum.filmorate.model.User;
 import java.util.List;
 
 public interface UserStorage {
+
     User create(User user);
+
     User update(@Valid User user);
+
     List<User> getAll();
+
     User get(long userId);
+
     void checkId(long id);
 }
