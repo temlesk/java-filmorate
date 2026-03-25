6a80b3b03b1be3fdf76a3eb6dabf6571775eb065
Temlesk <Teml1010>
2026-03-25 21:02:20 +0300

    commit №10 add-friends-likes

--- src/main/java/ru/yandex/practicum/filmorate/strorage/memory/InMemoryUserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/strorage/memory/InMemoryUserStorage.java
@@ -1,7 +1,6 @@
 package ru.yandex.practicum.filmorate.strorage.memory;
 
 import org.springframework.stereotype.Component;
-import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.strorage.UserStorage;
 
