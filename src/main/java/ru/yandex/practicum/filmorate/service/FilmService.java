8ffb905b3a78d9de55f987c41e443b2584782c87
Temlesk <Teml1010>
2026-03-26 00:25:39 +0300

    commit №11 add-friends-likes

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -3,9 +3,11 @@ package ru.yandex.practicum.filmorate.service;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.strorage.FilmStorage;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -16,10 +18,18 @@ public class FilmService {
     private final UserService userService;
 
     public Film create(@Valid Film film) {
+        checkDate(film);
         return filmStorage.create(film);
     }
 
+    public void checkDate(@Valid Film film) {
+        if (film.getReleaseDate().isBefore(LocalDate.of(1895, 12, 28))) {
+            throw new ValidationException("Некорректная дата релиза фильма");
+        }
+    }
+
     public Film update(@Valid Film film) {
+        checkDate(film);
         return filmStorage.update(film);
     }
 
