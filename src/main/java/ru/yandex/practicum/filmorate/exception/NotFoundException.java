9038b2ba18ab0b428339d05bc46b416905441dec
Temlesk <Teml1010>
2026-04-22 21:40:00 +0300

    commit add-database №3

--- src/main/java/ru/yandex/practicum/filmorate/exception/NotFoundException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/NotFoundException.java
@@ -4,7 +4,7 @@ import lombok.Getter;
 
 @Getter
 public class NotFoundException extends RuntimeException {
-    public NotFoundException (String message) {
+    public NotFoundException(String message) {
         super(message);
     }
 }
