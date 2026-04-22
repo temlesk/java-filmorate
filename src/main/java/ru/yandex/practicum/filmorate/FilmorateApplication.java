4a7097f68d569a24830a0dfd93c82b52770a42be
Temlesk <Teml1010>
2026-04-22 21:38:11 +0300

    commit add-database №2

--- src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
+++ src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
@@ -3,7 +3,6 @@ package ru.yandex.practicum.filmorate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.stereotype.Component;
 
 @SuppressWarnings("checkstyle:all")
 @SpringBootApplication
