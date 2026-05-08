29118118df48ef33411820ea4053f1b1a04c08d6
Temlesk <Teml1010>
2026-05-08 19:02:20 +0300

    commit add-database №43

--- src/main/java/ru/yandex/practicum/filmorate/exception/handler/ErrorHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/handler/ErrorHandler.java
@@ -2,13 +2,11 @@ package ru.yandex.practicum.filmorate.exception.handler;
 
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
-import ru.yandex.practicum.filmorate.exception.QueryParameterNotValidException;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 
 import java.util.Map;
