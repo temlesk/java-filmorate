b739632f7cb540f91ad69643df08a1fba2d82f7d
Temlesk <Teml1010>
2026-05-13 22:37:41 +0300

    commit add-database №51

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -34,7 +34,7 @@ public class FilmController {
         return filmService.getAll();
     }
 
-    @PutMapping("/{filmId}/like/{likerId}")
+    @PutMapping("/{id}/like/{likerId}")
     public void addLike(@PathVariable long filmId, @PathVariable long likerId) {
         filmService.addLike(filmId, likerId);
     }
